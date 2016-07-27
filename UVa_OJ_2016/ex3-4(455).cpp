aa6ead4469eb093e57a10bc2a14ebe4853955ce6
chenkuaan <chen1511@foxmail.com>
2016-07-27 18:11:47 +0800

    第一次练习第二次提交

--- UVa_OJ_2016/ex3-4(455).cpp
+++ UVa_OJ_2016/ex3-4(455).cpp
@@ -3,13 +3,18 @@
 //
 
 #include <cstdio>
+#include <cstring>
 
 int main(){
     int t;
     scanf("%d", &t);
     while(t--){
+        int period;
         char s[100];
         scanf("%s", s);
-        int period = 0;
+        int len = strlen(s);
+        for (int k = 1; k <= len / 2; k++){
+
+        }
     }
 }
